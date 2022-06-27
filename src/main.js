de202ef4a9a02f828b6fa8cb7c29d9068f6faef8
momoyu001 <lixia1220x@163.com>
2022-06-27 13:09:49 +0800

    完善工程环境，安装第三方插件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import Router from './router/index';
+import { Button } from 'vant';
+import 'lib-flexible/flexible'
+import 'vant/lib/index.css'; // 全局引入样式
 
 const app = createApp(App);
 
 app.use(Router)
+    .use(Button)
     .mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home-module">
     首页
+    <van-button type="primary">测试按需引入按钮</van-button>
   </div>
 </template>
 
